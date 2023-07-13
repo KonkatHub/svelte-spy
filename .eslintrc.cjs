e3d41770dfa95249832e98b73a75d84f12066fe6
Mathis Côté <mathis.cote1@outlook.com>
2023-07-12 21:29:42 -0400

    chore: format all files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,30 +1,73 @@
 module.exports = {
-	root: true,
-	extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:svelte/recommended',
-		'prettier'
-	],
-	parser: '@typescript-eslint/parser',
-	plugins: ['@typescript-eslint'],
-	parserOptions: {
-		sourceType: 'module',
-		ecmaVersion: 2020,
-		extraFileExtensions: ['.svelte']
-	},
-	env: {
-		browser: true,
-		es2017: true,
-		node: true
-	},
-	overrides: [
-		{
-			files: ['*.svelte'],
-			parser: 'svelte-eslint-parser',
-			parserOptions: {
-				parser: '@typescript-eslint/parser'
-			}
-		}
-	]
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:svelte/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2020,
+    extraFileExtensions: ['.svelte'],
+  },
+  env: {
+    browser: true,
+    es2017: true,
+    node: true,
+  },
+  overrides: [
+    {
+      files: ['*.svelte'],
+      parser: 'svelte-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+      },
+    },
+  ],
+  rules: {
+    'arrow-spacing': ['warn', { before: true, after: true }],
+    'brace-style': ['error', '1tbs', { allowSingleLine: true }],
+    'comma-dangle': ['error', 'only-multiline'],
+    'comma-spacing': 'error',
+    'comma-style': 'error',
+    curly: ['error', 'multi-line', 'consistent'],
+    'dot-location': ['error', 'property'],
+    'handle-callback-err': 'off',
+    indent: ['error', 2],
+    'keyword-spacing': 'error',
+    'max-nested-callbacks': ['error', { max: 4 }],
+    'max-statements-per-line': ['error', { max: 1 }],
+    'no-console': 'off',
+    'no-empty-function': 'error',
+    'no-floating-decimal': 'error',
+    'no-inline-comments': 'off',
+    'no-lonely-if': 'error',
+    'no-multi-spaces': 'error',
+    'no-multiple-empty-lines': ['error', { max: 2, maxEOF: 1, maxBOF: 0 }],
+    'no-trailing-spaces': ['error'],
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'no-var': 'error',
+    'object-curly-spacing': ['error', 'always'],
+    'prefer-const': 'error',
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'space-before-blocks': 'error',
+    'space-before-function-paren': [
+      'error',
+      {
+        anonymous: 'always',
+        named: 'never',
+        asyncArrow: 'always',
+      },
+    ],
+    'space-in-parens': 'error',
+    'space-infix-ops': 'error',
+    'space-unary-ops': 'error',
+    'spaced-comment': 'error',
+    yoda: 'error',
+  },
 };
